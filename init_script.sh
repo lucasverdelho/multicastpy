6f9d55c199f6a5ed77a67ea923207b97a98141e2
LucasVerdelho <PG50221@alunos.uminho.pt>
2023-11-13 16:46:27 +0000

    script copying files to correct node types

--- init_script.sh
+++ init_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Set the process ID
-PROCESS_ID="38297" 
-# Ainda nao sei como automatizar isto mas basicly iniciamos a rede
-# e vemos qual o PID do pycore 
+PROCESS_ID="38297"
 
 # List of nodes for which you want to run vcmd commands
 NODES=("n1" "n2" "n3" "n4" "n5" "n6" "n7" "n8" "n9" "n10" "n11" "n12" "n13" "n14" "n15" "n16" "n17")
+
+# Source folder for file copies
+SOURCE_FOLDER="/home/core/multicastpy"
 
 # Your custom commands
 CUSTOM_COMMAND_INIT_NODE="ls"
@@ -15,17 +16,24 @@ CUSTOM_COMMAND_INIT_SERVER="echo 'Hello from Node (Server)'"
 
 # Iterate over nodes and run vcmd commands
 for NODE in "${NODES[@]}"; do
-    vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- $(
-        case $NODE in
+    vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+        cp ${SOURCE_FOLDER}/netconfig.txt .
+        "
+    case $NODE in
         "n1"|"n2"|"n3"|"n4"|"n5"|"n6"|"n7"|"n8"|"n9"|"n10")
-            echo $CUSTOM_COMMAND_INIT_NODE
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/node.py .
+                "
             ;;
         "n11"|"n12"|"n13"|"n14"|"n15"|"n16")
-            echo $CUSTOM_COMMAND_INIT_CLIENT
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/client.py .
+                "
             ;;
         "n17")
-            echo $CUSTOM_COMMAND_INIT_SERVER
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/server.py .
+                "
             ;;
-        esac
-    )
+    esac
 done
