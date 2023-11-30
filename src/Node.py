d800d7a30516470a239298d47993201916a30161
lucas <a.lucas.s.verdelho@gmail.com>
2023-11-30 15:21:47 +0000

    Node logic started

--- src/Node.py
+++ src/Node.py
@@ -7,7 +7,8 @@ class Node:
     
 
     neighbours = []
-    rp_node_ip = "0.0.0.0"
+    streaming_content = {}
+    rp_node_ip = "0.0.0.0:5000"
 
     def main(self):
         try:
@@ -16,7 +17,7 @@ class Node:
         except (IndexError, ValueError):
             print("[Usage: Node.py node_port node_number]\n")
 
-        self.neighbours = self.read_ips_from_file()
+        self.neighbours = self.read_ips_from_file(NODE_NUMBER)
 
         nodeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         nodeSocket.bind(('', NODE_PORT))
@@ -34,8 +35,10 @@ class Node:
 
             request_socket.close()            
 
-    def read_ips_from_file(self):
-        with open('neighbours.txt') as f:
+    def read_ips_from_file(self, NODE_NUMBER):
+        filename = "node" + NODE_NUMBER + ".txt"
+
+        with open(filename) as f:
             for line in f:
                 self.neighbours.append
 
@@ -44,12 +47,13 @@ class Node:
         print(f"Received request: {request}")
 
         if request == "CONTENT_REQUEST":
-            self.handle_content_request(request, request_socket, request_address)
+            threading.Thread(target=self.handle_content_request, args=(request, request_socket, request_address)).start()
         elif request == "LOCATE_RP_REQUEST":
-            self.handle_locate_request(request, request_socket, request_address)
+            threading.Thread(target=self.handle_locate_request, args=(request, request_socket, request_address)).start()
         else:
             print("Invalid request.")
 
+        return
 
     def handle_content_request(self, request, request_socket, request_address):
         
@@ -57,8 +61,16 @@ class Node:
         
         content_requested = request.split(";")
 
+        if self.streaming_content.contains(content_requested):
+            # Copy each packet of data from the stream to the request socket
+            
 
-        
+
+        # Check if the node is connected to the RP
+        if self.neighbours.contains(self.rp_node_ip):
+            print("Node is connected to the RP.")
+            self.send_content_request(content_requested, request_socket, request_address)
+
 
         # Send Request to RP for the content
         # TODO 
@@ -75,6 +87,14 @@ class Node:
         # 2. Listen for the response of a Node who is neighbour to the RP
 
 
+
+    def send_content_request(self, content_requested, request_socket, request_address):
+        temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        temp_socket.connect((self.rp_node_ip, 5000))
+        request = "CONTENT_REQUEST;" + content_requested
+        temp_socket.send(request.encode())
+
+        
 
 if __name__ == "__main__":
     (Node()).main()
