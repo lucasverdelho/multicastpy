231a1fde67980975caeede6aff609cfb39cb8910
LucasVerdelho <PG50221@alunos.uminho.pt>
2023-12-04 22:45:26 +0000

    Init Script Fixed, added OVERLAY nodes neighbours, updated topology

--- init_script2.sh
+++ init_script2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the process ID
-PROCESS_ID="35307"
+PROCESS_ID="36629"
 
 # List of nodes for which you want to run vcmd commands
 NODES=("n1" "n2" "n3" "n4" "n5" "n6" "n7" "n8" "n9" "n10" "n11" "n12" "n13" "n14" "n15" "n16" "n17")
@@ -12,19 +12,34 @@ SOURCE_FOLDER="/home/core/multicastpy"
 # Your custom commands
 CUSTOM_COMMAND_INIT_NODE="ls"
 CUSTOM_COMMAND_INIT_CLIENT="echo 'Hello from Node (Client)'"
-CUSTOM_COMMAND_INIT_SERVER="echo 'Hello from Node (Server)'"
+CUSTOM_COMMAND_INIT_SERVER="python3 Server.py 6000"
 
 # Iterate over nodes and run vcmd commands
 for NODE in "${NODES[@]}"; do
     vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
-        cp -r ${SOURCE_FOLDER}/Testing/* .
+        cp ${SOURCE_FOLDER}/Testing/neighbour_lists/* .
         "
     case $NODE in
-        "n1"|"n2"|"n3"|"n4"|"n5"|"n6"|"n7"|"n8"|"n9"|"n10")
+        "n1"|"n2"|"n5"|"n10") # Overlay Nodes
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/Testing/*.py .
+                "
             ;;
         "n11"|"n12"|"n13"|"n14"|"n15"|"n16")
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/Testing/Client.py .
+                cp ${SOURCE_FOLDER}/Testing/RtpPacket.py .
+                cp ${SOURCE_FOLDER}/Testing/VideoStream.py .
+                cp ${SOURCE_FOLDER}/Testing/testClient.py .
+                "
             ;;
         "n17")
+            vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
+                cp ${SOURCE_FOLDER}/Testing/Server.py .
+                cp ${SOURCE_FOLDER}/Testing/Serverworker.py .
+                cp ${SOURCE_FOLDER}/Testing/VideoStream.py .
+                cp ${SOURCE_FOLDER}/Testing/RtpPacket.py .
+                "
             ;;
     esac
 done
@@ -32,7 +47,7 @@ done
 # Iterate over nodes and run vcmd commands for custom initialization commands
 for NODE in "${NODES[@]}"; do
     case $NODE in
-        "n1"|"n2"|"n3"|"n4"|"n5"|"n6"|"n7"|"n8"|"n9"|"n10")
+        "n1"|"n2"|"n5"|"n10")
             vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
                 $CUSTOM_COMMAND_INIT_NODE
                 "
@@ -44,7 +59,7 @@ for NODE in "${NODES[@]}"; do
             ;;
         "n17")
             vcmd -c "/tmp/pycore.${PROCESS_ID}/${NODE}" -- bash -c "
-                $CUSTOM_COMMAND_INIT_SERVER
+                $CUSTOM_COMMAND_INIT_CLIENT
                 "
             ;;
     esac
