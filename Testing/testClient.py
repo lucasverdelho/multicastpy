734ef5117f5eaa5c5530d79f5c4ebf166906b6c4
LucasVerdelho <PG50221@alunos.uminho.pt>
2023-12-05 15:31:17 +0000

    multicast is working !! TODO, handle watching the video, cv2 not recognized in core

--- Testing/testClient.py
+++ Testing/testClient.py
@@ -1,7 +1,7 @@
 import socket
 import struct
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 
 def main():
     # Replace this value with the actual multicast group address and port in the format '224.1.1.1-5001'
@@ -25,7 +25,7 @@ def main():
     print(f"Client joined multicast group: {multicast_group}:{multicast_port}")
 
     # OpenCV window for displaying video
-    cv2.namedWindow("Video Stream", cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("Video Stream", cv2.WINDOW_NORMAL)
 
     # Buffer for accumulating data until a complete JPEG frame is received
     data_buffer = b''
@@ -37,41 +37,42 @@ def main():
             # Print information about received data
             print(f"Received data from {address}")
             print(f"Data length: {len(data)}")
+            print(f"Data: {data}")
 
-            # Accumulate data in the buffer
-            data_buffer += data
+            # # Accumulate data in the buffer
+            # data_buffer += data
 
-            # Check for the JPEG frame boundaries
-            if b'\xff\xd8' in data_buffer and b'\xff\xd9' in data_buffer:
-                # Find the start and end of the JPEG frame
-                frame_start = data_buffer.find(b'\xff\xd8')
-                frame_end = data_buffer.find(b'\xff\xd9') + 2
+            # # Check for the JPEG frame boundaries
+            # if b'\xff\xd8' in data_buffer and b'\xff\xd9' in data_buffer:
+            #     # Find the start and end of the JPEG frame
+            #     frame_start = data_buffer.find(b'\xff\xd8')
+            #     frame_end = data_buffer.find(b'\xff\xd9') + 2
 
-                # Extract the JPEG frame
-                jpeg_frame = data_buffer[frame_start:frame_end]
+            #     # Extract the JPEG frame
+            #     jpeg_frame = data_buffer[frame_start:frame_end]
 
-                try:
-                    # Decode the JPEG frame with OpenCV
-                    frame = cv2.imdecode(np.frombuffer(jpeg_frame, dtype=np.uint8), cv2.IMREAD_COLOR)
+            #     try:
+            #         # Decode the JPEG frame with OpenCV
+            #         frame = cv2.imdecode(np.frombuffer(jpeg_frame, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-                    # Display the video frame
-                    cv2.imshow("Video Stream", frame)
+            #         # Display the video frame
+            #         cv2.imshow("Video Stream", frame)
 
-                    # Reset the buffer after extracting a complete frame
-                    data_buffer = data_buffer[frame_end:]
-                except Exception as decode_error:
-                    print(f"Error decoding image: {decode_error}")
+            #         # Reset the buffer after extracting a complete frame
+            #         data_buffer = data_buffer[frame_end:]
+            #     except Exception as decode_error:
+            #         print(f"Error decoding image: {decode_error}")
 
-            # Break the loop if 'q' key is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # # Break the loop if 'q' key is pressed
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
 
     except Exception as main_error:
         print(f"Error in main loop: {main_error}")
 
     finally:
         # Release OpenCV window and close the socket
-        cv2.destroyAllWindows()
+        # cv2.destroyAllWindows()
         client_socket.close()
 
 if __name__ == "__main__":
